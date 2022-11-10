861488803c80eba9a7395d946d59851be7b2ffea
Flynn McCallum <102677191+flagdanger@users.noreply.github.com>
2022-11-10 18:00:06 -0500

    rearranged database
    ok so moved the data into new folders based on desired result/same chord. Found this to be too tedious so made a program to do it for me. Need more entries to make the ai more accurate, so rn I'm going back into a daw and making more soundsets of different intruments then moving them using the program into each chord folder. Hopefully trying to get up to around 1k files per chord. (Which is a lot with 4 chords for every 12 root notes)

---
+++ quickersorting.py
@@ -0,0 +1,272 @@
+import shutil
+import fnmatch
+import os
+import pathlib
+
+Path1 = "C:/Users/Flynn/Documents/GitHub/Adag.io/"
+DPath = "C:/Users/Flynn/Documents/GitHub/Adag.io/dataset/"
+
+original_folder = 'dataset_by_instrument/orchestra/'
+
+endfolders = ["A Major","A# Major","B Major","C Major","C# Major",
+              "D Major","D# Major","E Major","F Major","F# Major",
+              "G Major","G# Major",
+              "A Minor","A# Minor","B Minor","C Minor","C# Minor",
+              "D Minor","D# Minor","E Minor","F Minor","F# Minor",
+              "G Minor","G# Minor",
+              "A Major7", "A# Major7", "B Major7", "C Major7", "C# Major7",
+              "D Major7", "D# Major7", "E Major7", "F Major7", "F# Major7",
+              "G Major7", "G# Major7",
+              "A Minor7", "A# Minor7", "B Minor7", "C Minor7", "C# Minor7",
+              "D Minor7", "D# Minor7", "E Minor7", "F Minor7", "F# Minor7",
+              "G Minor7", "G# Minor7"
+              ]
+data_dir = pathlib.Path(original_folder)
+for file in os.listdir(original_folder):
+    if fnmatch.fnmatch(file, '01*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[0] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '02*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[1] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '03*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[2] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '04*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[3] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '05*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[4] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '06*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[5] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '07*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[6] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '08*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[7] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '09*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[8] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '10*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[9] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '11*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[10] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '12*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[11] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+####################### end of major ###############################
+    if fnmatch.fnmatch(file, '14*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[12] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '15*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[13] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '16*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[14] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '17*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[15] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '18*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[16] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '19*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[17] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '20*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[18] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '21*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[19] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '22*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[20] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '23*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[21] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '24*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[22] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '25*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[23] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    ########################## end minor  ##########################
+    if fnmatch.fnmatch(file, '27*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[24] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '28*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[25] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '29*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[26] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '30*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[27] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '31*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[28] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '32*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[29] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '33*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[30] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '34*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[31] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '35*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[32] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '36*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[33] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '37*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[34] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '38*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[35] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+########################## end of maj7 ##################################
+    if fnmatch.fnmatch(file, '40*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[36] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '41*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[37] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '42*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[38] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '43*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[39] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '44*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[40] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '45*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[41] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '46*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[42] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '47*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[43] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '48*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[44] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '49*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[45] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '50*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[46] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+    if fnmatch.fnmatch(file, '51*'):
+        source = Path1 + original_folder + file
+        destination = DPath + endfolders[47] + "/" + file
+        shutil.move(source, destination)
+        print('Moved:', file)
+####################### end of min 7 #########################
+
+
+
--- thedata.py
+++ thedata.py
@@ -1,59 +1,146 @@
 import os
+import pathlib
 from platform import platform
 import numpy
+import tensorflow as tf
 from tensorflow import keras
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+from keras import layers
+from keras import models
 
 
+seed = 99
+tf.random.set_seed(seed)
+numpy.random.seed(seed)
 
-def getFeatures(file):
+def getChroma(file):
     y, sr = librosa.load(file, offset=0, duration=1.0)
     y_harm = librosa.effects.harmonic(y=y, margin=4)
-    chroma = librosa.feature.chroma_cqt(y=y_harm, sr=sr, fmin=65.4, threshold=.4, n_chroma = 12)
+    chroma = librosa.feature.chroma_cqt(y=y_harm, sr=sr, fmin=65.4, threshold=.4, n_chroma=12)
     chroma_array = numpy.array(chroma)
-    chroma_mean = chroma_array.mean(axis=1)
-    chroma_min = numpy.min(chroma_array)
-    chroma_max = numpy.max(chroma_array)
+    # trying to use 2d array now
+    #chroma_mean = chroma_array.mean(axis=1)
+    #chroma_min = numpy.min(chroma_array)
+    #chroma_max = numpy.max(chroma_array)
 
-    feature = numpy.concatenate((chroma_mean,chroma_min,chroma_max))
+    feature = chroma_array
+    print(feature)
     return feature
 
 
 dataset = 'dataset'
+data_dir = pathlib.Path(dataset)
+
 features = []
 labels = []
-folders = ["Piano_Major","Piano_Minor"]
-majorRoots = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
-minorRoots = ["A#m","Am","Bm","C#m","Cm","D#m","Dm","Em","F#m","Fm","G#m","Gm"]    
+chords = []
+# This works and fills chords with the names of every folder
+for name in os.listdir(dataset):
+    chords.append(name)
 
-for type in folders: 
-    x = 0
-    for root in os.listdir(dataset+"/"+type):
-        filename = dataset+"/"+type+"/"+root
+for chord in chords:
+    print("Calculating features for chord : " + chord)
+    for file in os.listdir(dataset+"/"+chord):
+        file_path = dataset+"/"+chord+"/"+file
 
-        features.append(getFeatures(filename))
-        if type=="Piano_Major":
-            label = majorRoots[x]
-        else:
-            label = minorRoots[x]
-        x += 1
+        features.append(getChroma(file_path))
+        label = chords.index(chord)
         labels.append(label)
-        
 
-#the collection of features has been made
-#now we shuffle them to be used as training for the model
+print(labels)
 
-permu = numpy.random.permutation(300)
-features = numpy.array(features)[permu]
-labels = numpy.array(labels)[permu]
+permutations = numpy.random.permutation(288)
+features = numpy.array(features)[permutations]
+labels = numpy.array(labels)[permutations]
 
-fTrain = features[0:180]
-lTrain = labels[0:180]
+features_train = features[0:180]
+labels_train = labels[0:180]
 
-fVal = features[180:240]
-lVal = labels[180:240]
+features_val = features[180:240]
+labels_val = labels[180:240]
 
-fTest = features[240:300]
-lTest = labels[240:300]
+features_test = features[240:288]
+labels_test = labels[240:288]
+
+####################################################
+
+#inputs = keras.Input(shape=(12,44), name="feature")
+#x = keras.layers.Dense(288, activation="relu", name="dense_1")(inputs)
+#x = keras.layers.Dense(288, activation="relu", name="dense_2")(x)
+#outputs = keras.layers.Dense(48, activation="softmax", name="predictions")(x)
+
+#model = keras.Model(inputs=inputs, outputs=outputs)
+
+
+#iffy statement
+input_shape = features_test[1].shape[1:]
+print('Input shape:', input_shape)
+num_labels = len(chords)
+norm_layer = layers.Normalization()
+# Fit the state of the layer to the spectrograms
+# with `Normalization.adapt`.
+norm_layer.adapt(data=features_train.map(map_func=lambda spec, label: spec))
+
+model = models.Sequential([
+    layers.Input(shape=input_shape),
+    # Downsample the input.
+    layers.Resizing(32, 32),
+    # Normalize.
+    norm_layer,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(num_labels),
+])
+
+model.summary()
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+model.fit(x=features_train.tolist(),y=labels_train.tolist(),verbose=1,validation_data=(features_val.tolist() , labels_val.tolist()), epochs=64)
+
+
+score = model.evaluate(x=features_test.tolist(),y=labels_test.tolist(), verbose=0)
+print('Accuracy : ' + str(score[1]*100) + '%')
+
+
+#majorRoots = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+#minorRoots = ["A#m", "Am", "Bm", "C#m", "Cm", "D#m", "Dm", "Em", "F#m", "Fm", "G#m", "Gm"]
+
+#for type in folders:
+#    x = 0
+#    for root in os.listdir(dataset + "/" + type):
+#        filename = dataset + "/" + type + "/" + root
+#
+#        features.append(getFeatures(filename))
+#        if type == "Piano_Major":
+#            label = majorRoots[x]
+#        else:
+#            label = minorRoots[x]
+#        x += 1
+#        labels.append(label)
+
+# the collection of features has been made
+# now we shuffle them to be used as training for the model
+
+#permu = numpy.random.permutation(300)
+#features = numpy.array(features)[permu]
+#labels = numpy.array(labels)[permu]
+
+#fTrain = features[0:180]
+#lTrain = labels[0:180]
+
+#fVal = features[180:240]
+#lVal = labels[180:240]
+
+#fTest = features[240:300]
+#lTest = labels[240:300]
